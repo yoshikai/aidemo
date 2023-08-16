52b598ab005f6dba8d2b689d4dbb1884b0a23676
yoshikai <yoshikai.tsuyoshi@gmail.com>
2023-08-16 15:17:12 +0900

    コントローラーの追加とpom.xmlファイルの更新

--- src/main/java/jp/co/solenas/aidemo/controller/OpenAiController.java
+++ src/main/java/jp/co/solenas/aidemo/controller/OpenAiController.java
@@ -1,49 +1,11 @@
 package jp.co.solenas.aidemo.controller;
 
-import jp.co.solenas.aidemo.request.BotRequest;
-import jp.co.solenas.aidemo.request.Message;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.*;
-import jp.co.solenas.aidemo.response.BotResponse;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.List;
-
-@RestController
 public class OpenAiController {
-    @Autowired
-    private RestTemplate restTemplate;
 
-    @Value("${openai.model}")
-    private String model;
-
-    @Value("${openai.max-completions}")
-    private int maxCompletions;
-
-    @Value("${openai.temperature}")
-    private double temperature;
-
-    @Value("${openai.maxTokens}")
-    private int maxTokens;
-
-    @Value("${openai.api.url}")
-    private String apiUrl;
-
-    @GetMapping("/chat")
-    public BotResponse chat(@RequestParam("prompt") String prompt) {
-        BotRequest request = new BotRequest(model,
-                List.of(new Message("user", prompt)),
-                maxCompletions,
-                temperature,
-                maxTokens);
-
-        BotResponse response = restTemplate.postForObject(apiUrl, request, BotResponse.class);
-        return response;
-    }
-
-    @PostMapping("/chat")
-    public BotResponse postPrompt(@RequestParam("prompt") String prompt){
-        return chat(prompt);
+    @GetMapping("/")
+    public String index(){
+        return "index";
     }
 }
